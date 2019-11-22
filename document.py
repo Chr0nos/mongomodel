a45dc946e8903692da4ceb456680a60fbe3e70fc
Sébastien NICOLET <snicolet@student.42.fr>
2019-11-22 18:03:55 +0100

    added methods on documents and fields stating to work fine

--- document.py
+++ document.py
@@ -1,7 +1,10 @@
 from bson import ObjectId
 import pymongo
 import re
-from . import db
+# from . import db
+
+client = pymongo.MongoClient(host='10.8.0.1')
+db: pymongo.database.Database = client.test
 
 
 class Document:
@@ -10,27 +13,44 @@ class Document:
     meta = None
 
     def __init__(self, **kwargs):
-        # TODO : remove this fuckery
-        for key, value in kwargs:
-            setattr(self, key, value)
-        self.meta = kwargs.keys()
+        for key, value in kwargs.items():
+            field = object.__getattribute__(self, key)
+            field.set_value(value)
+        self.fields = tuple(kwargs.keys())
+
+    def __getattribute__(self, name):
+        attribute = object.__getattribute__(self, name)
+        if isinstance(attribute, Field):
+            return attribute.value
+        return attribute
+
+    def __setattr__(self, name, value):
+        try:
+            attribute = self.raw_attr(name)
+        except AttributeError:
+            attribute = None
+        if attribute and isinstance(attribute, Field):
+            return attribute.set_value(value)
+        return object.__setattr__(self, name, value)
 
     def save(self):
         """Update or insert the current document to the database if needed
         then return the response from the database
         """
-        if not self.id:
+        if not self._id:
             response = self.cursor.insert_one(self.to_dict())
             self._id = response.inserted_id
             return response
-        return self.cursor.update_one({'_id': self.id}, self.to_dict())
+        return self.cursor.update_one({'_id': self._id}, self.to_dict())
 
     def delete(self):
         if not self._id:
             return
         return self.cursor.delete_one({'_id': self._id})
 
-    def find(self, match: dict, one=True):
+    def find(self, match: dict = None, one=True):
+        if match is None:
+            match = {}
         if one:
             return self.cursor.find_one(match)
         return self.cursor.find(match)
@@ -40,32 +60,43 @@ class Document:
         return db[self.collection]
 
     def to_dict(self) -> dict:
-        raise NotImplementedError
+        return dict({k: getattr(self, k) for k in self.fields})
+
+    def raw_attr(self, name):
+        return object.__getattribute__(self, name)
+
+    def is_valid(self) -> bool:
+        for field_name in self.fields:
+            field = object.__getattribute__(self, field_name)
+            if not field.is_valid():
+                return False
+        return True
 
 
 class Field:
-    def __init__(self, data):
-        self.data = data
+    value = None
+
+    def set_value(self, value):
+        self.value = value
 
 
 class StringField(Field):
-    def __init__(self, data, maxlen=None):
-        super().__init__(data)
+    def __init__(self, maxlen=None):
         self.maxlen = maxlen
 
     def is_valid(self) -> bool:
-        if not isinstance(self.data, str):
+        if not isinstance(self.value, str):
             return False
         if not self.maxlen:
             return True
-        return len(self.data) <= self.maxlen
+        return len(self.value) <= self.maxlen
 
 
 class EmailField(StringField):
     def is_valid(self) -> bool:
-        if not super().is_valid(self.data):
+        if not super().is_valid():
             return False
-        if not re.match(r'[\w]+@[\w]+', self.data):
+        if not re.match(r'[\w]+@[\w]+', self.value):
             return False
         return True
 
