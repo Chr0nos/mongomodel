f902fc85c3d7b7671586534d7d80acc70f266373
Sébastien NICOLET <snicolet@student.42.fr>
2020-01-02 11:59:27 +0100

    imrpoved unit tests for querysets

--- tests/test_queryset.py
+++ tests/test_queryset.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mock import patch
+from mock import patch, Mock
 from mongomodel.queryset import QuerySet
 
 
@@ -34,11 +34,28 @@ class TestQuerySet:
         lst = QuerySet.sort_instruction(['date', '-age', 'a', '-b'])
         assert lst == [('date', 1), ('age', -1), ('a', 1), ('b', -1)]
 
-    @pytest.mark.skip('work in progress')
-    @patch('mongomodel.queryset.QuerySet', spec=True)
-    def test_count(self, mock_model):
-        count = mock_model.model.get_collection.return_value.count_documents
+    def test_count(self):
+        fake_model = Mock()
+        count = fake_model.get_collection.return_value.count_documents
         count.return_value = 1234
-        response = mock_model('test').count()
+        response = QuerySet(fake_model).count()
         assert response == 1234
         count.assert_called_once()
+
+    @patch('mongomodel.queryset.QuerySet')
+    def test_raw(self, mock_qs):
+        cursor = object()
+        model = Mock()
+        model.find_raw.return_value = cursor
+
+        qs = QuerySet(model)
+        qs._get_cursor = Mock()
+        qs._get_cursor.return_value = cursor
+        assert qs.raw(1, ['name'], 3) == cursor
+        qs._get_cursor.assert_called_with(cursor, ['name'], 3, 1)
+
+    def test_raw_without_model(self):
+        from mongomodel.queryset import MissingModelError
+
+        with pytest.raises(MissingModelError):
+            QuerySet().raw()
