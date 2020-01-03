0728bd76ab5f6229edea95461e0afdc99b9aa775
Sébastien NICOLET <snicolet@student.42.fr>
2020-01-03 21:23:20 +0100

    updated setup to 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_long_description() -> str:
 
 setuptools.setup(
     name='mongomodel',
-    version='0.2.2',
+    version='0.2.3',
     scripts=[],
     author="Sébastien Nicolet",
     author_email="snicolet@student.42.fr",
