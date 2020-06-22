2fff651f125530187ccc20ade31e815390363f5e
Chr0nos <snicolet@student.42.fr>
2020-06-22 22:47:20 +0200

    it's now possible to pass connect=True to setup_database

--- mongomodel/conf.py
+++ mongomodel/conf.py
@@ -5,7 +5,8 @@ from pymongo import MongoClient
 def setup_database(hostname='10.8.0.1', database='test', **kwargs):
     import mongomodel
 
-    client = MongoClient(host=hostname, connect=False, **kwargs)
+    kwargs.setdefault('connect', False)
+    client = MongoClient(host=hostname, **kwargs)
     mongomodel.client = client
     mongomodel.db = client[database]
     mongomodel.document.db = mongomodel.db
