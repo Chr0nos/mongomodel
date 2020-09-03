98f51b2ca00b18e7d51a40ef1fa3eba5cf078048
Sébastien NICOLET <snicolet@student.42.fr>
2020-09-04 00:25:22 +0200

    [0.4.7] moved low levels functions from QuerySet to QuerySetBase, added a .new method into Database, the connect method now return the Database

--- mongomodel/__init__.py
+++ mongomodel/__init__.py
@@ -6,19 +6,32 @@ class Database:
     def __init__(self):
         self.connect(host='localhost', connect=False)
 
-    def connect(self, **kwargs):
+    def connect(self, **kwargs) -> 'Database':
         kwargs.setdefault('connect', True)
         db_name = kwargs.pop('db', 'test')
 
         self.client = pymongo.MongoClient(**kwargs)
         self.db: pymongo.database.Database = getattr(self.client, db_name)
+        return self
 
     def __repr__(self):
         return f'<Database: {self.client.HOST}: {self.db}>'
 
+    def new(self, **kwargs) -> 'Database':
+        return Database().connect()
+
 
 database = Database()
 
-from .field import Field, StringField, EmailField, RegexField, IntegerField, \
-                   TypeField, FloatField, DateTimeField, BoolField
-from .document import Document, QuerySet
+from .field import (
+    Field,
+    StringField,
+    EmailField,
+    RegexField,
+    IntegerField,
+    TypeField,
+    FloatField,
+    DateTimeField,
+    BoolField
+)
+from .document import Document, QuerySet  # noqa: F401
--- mongomodel/queryset.py
+++ mongomodel/queryset.py
@@ -14,7 +14,7 @@ class TooManyResults(Exception):
     pass
 
 
-class QuerySet:
+class QuerysetBase:
     query = {}
     # must be a Model class, not an instance
     model = None
@@ -125,7 +125,98 @@ class QuerySet:
                 pass
         return path, raw_value
 
+    def __iter__(self):
+        raise NotImplementedError
+
+    def __add__(self, b: 'QuerySet') -> 'QuerySet':
+        instance = self.copy()
+        dict_deep_update(instance.query, b.query, on_conflict=merge_values)
+        if not instance.model and b.model:
+            instance.model = b.model
+        return instance
+
+    @staticmethod
+    def sort_instruction(order: List[str]) -> List[tuple]:
+        """Convert a list for format:
+        ['name', '-age'] to:
+        [('name': 1), ('age': -1)]
+        """
+        def generate_tuple(word) -> tuple:
+            if word.startswith('-'):
+                return (word[1:], -1)
+            return (word, 1)
+
+        return [generate_tuple(word) for word in order]
+
+    def get_collection_name(self) -> str:
+        try:
+            collection_name = self.model.collection
+        except AttributeError:
+            pass
+
+        if not collection_name:
+            collection_name = self.model.__name__.lower()
+        return collection_name
+
+    def count(self):
+        raise NotImplementedError
+
+    def create(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def raw(self, **kwargs):
+        raise NotImplementedError
+
+    def raw_all(self, **kwargs):
+        raise NotImplementedError
+
+    def all(self, **kwargs):
+        raise NotImplementedError
+
+    def first(self, **kwargs):
+        raise NotImplementedError
+
+    def find_one(self, **kwargs):
+        raise NotImplementedError
+
+    def find(self, filter: dict = None, **kwargs):
+        raise NotImplementedError
+
+    def get(self, **kwargs):
+        raise NotImplementedError
+
+    def distinct(self, key: str, **kwargs):
+        raise NotImplementedError
+
+    def delete(self):
+        raise NotImplementedError
+
+    def drop(self):
+        raise NotImplementedError
+
+    def values_list(self, fields: List[str], flat=False, noid=False):
+        raise NotImplementedError
+
+
+class QuerySet(QuerysetBase):
+    """The QuetySet is the bridge between mongodb and the models creation
+    eveyrthing in that class should not be related to one model but to `any` of
+    them.
+
+    This class implement the synchrone implementation with `pymongo`
+
+    it handle:
+    - sort
+    - limit
+    - offset
+    - query
+
+    you can override this class by creating one who inherit from it and put it
+    into `model.manager_class` attribute.
+    """
     def __iter__(self, **kwargs):
+        """Iterate over the matching models instances
+        """
         if not self.model:
             raise MissingModelError
         if self._sort:
@@ -138,6 +229,9 @@ class QuerySet:
             yield instance
 
     def count(self) -> int:
+        """Return the amount of matching elements.
+        sort/limit/offset are ignored
+        """
         if not self.model:
             raise MissingModelError
         return self.get_collection().count_documents(self.query)
@@ -196,31 +290,11 @@ class QuerySet:
         model_instance = self.model(**search[0])
         return model_instance
 
-    def __add__(self, b: 'QuerySet') -> 'QuerySet':
-        instance = self.copy()
-        dict_deep_update(instance.query, b.query, on_conflict=merge_values)
-        if not instance.model and b.model:
-            instance.model = b.model
-        return instance
-
     def distinct(self, key: str, **kwargs) -> List[Any]:
         if not self.model:
             raise MissingModelError
         return self.get_collection().distinct(
             key=key, query=self.query, **kwargs)
-
-    @staticmethod
-    def sort_instruction(order: List[str]) -> List[tuple]:
-        """Convert a list for format:
-        ['name', '-age'] to:
-        [('name': 1), ('age': -1)]
-        """
-        def generate_tuple(word) -> tuple:
-            if word.startswith('-'):
-                return (word[1:], -1)
-            return (word, 1)
-
-        return [generate_tuple(word) for word in order]
 
     def delete(self):
         if not self.model:
@@ -229,16 +303,6 @@ class QuerySet:
         cursor = self._get_cursor(collection.find(self.query))
         ids = cursor.distinct('_id')
         return collection.delete_many({'_id': {'$in': ids}})
-
-    def get_collection_name(self) -> str:
-        try:
-            collection_name = self.model.collection
-        except AttributeError:
-            pass
-
-        if not collection_name:
-            collection_name = self.model.__name__.lower()
-        return collection_name
 
     def get_collection(self) -> Collection:
         return self._db.db[self.get_collection_name()]
@@ -276,10 +340,12 @@ class QuerySet:
         projection = {f: True for f in fields}
         if noid:
             projection['_id'] = False
+
+        cursor = self._get_cursor(self.find_raw(projection=projection))
         if not flat:
-            return list(self.find_raw(projection=projection))
+            return list(cursor)
         assert len(fields) == 1, 'You can only have one field using flat=True'
         field_name = fields[0]
         return list([
-            value[field_name] for value in self.find_raw(projection=projection)
+            value[field_name] for value in cursor
         ])
